d476417dcd32bb5dfeabf16518c22f8c11e3cf7c
FER201m-SP22 <sontddeveloper@gmail.com>
2022-07-27 05:21:11 +0700

    done

--- 01-birthday-reminder/src/App.js
+++ 01-birthday-reminder/src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import data from './data';
-import List from './List';
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data";
+import List from "./List";
+import { useState } from "react";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people,setPeople]=useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3> {people.length} Birthdays To Day</h3>
+        <List people={people} />
+        <button onClick={()=> setPeople([])}>Clear All</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
