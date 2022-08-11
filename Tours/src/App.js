bae9eba6a8f07fb80cd0b7a35409e2b86d539fda
FER201m-SP22 <sontddeveloper@gmail.com>
2022-08-11 23:30:51 +0700

    add btn-refresh adn set properties for two btn

--- Tours/src/App.js
+++ Tours/src/App.js
@@ -7,7 +7,10 @@ const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading,setLoading]= useState(true);
   const [tours,setTours] = useState([]);
-
+  const removeTour = (id) =>{
+    const newTours=tours.filter((tour)=>tour.id!==id);
+    setTours(newTours);
+  };
 const fetchTours = async () =>{
   setLoading(true);
 
@@ -34,8 +37,18 @@ useEffect(() =>{
       </main>
     );
   }
+  if(tours.length === 0){
+    return (
+      <main>
+        <div class="title">
+          <h2>No tours left</h2>
+          <button class="refresh-btn" onClick={fetchTours}>Refresh</button>
+        </div>
+      </main>
+    );
+  }
   return <main>
-    <Tours tours={tours}/>
+    <Tours tours={tours} removeTour={removeTour} />
   </main>
 }
 
--- Tours/src/Tour.js
+++ Tours/src/Tour.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Tour = ({id,image,info,price,name}) => {
+const Tour = ({id,image,info,price,name,removeTour}) => {
   const[readMore,setReadMore]= useState(false);
   return <article className="single-tour" >
     <img src={image} alt={name}/>
@@ -15,7 +15,7 @@ const Tour = ({id,image,info,price,name}) => {
       </button>
       </p>
       
-      <button className="delete-btn">Not Interested</button>
+      <button className="delete-btn" onClick={()=> removeTour(id)}>Not Interested</button>
     </footer>
   </article>;
 };
