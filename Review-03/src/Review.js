0c41ad4fde844a9da1a2cbbd98c94d559c3b43e1
FER201m-SP22 <sontddeveloper@gmail.com>
2022-08-14 03:15:18 +0700

    set Math for button suprise to use random function

--- Review-03/src/Review.js
+++ Review-03/src/Review.js
@@ -24,9 +24,18 @@ const Review = () => {
   const prevPerson = () => {
     setIndex((index) => {
       let newIndex = index - 1;
-      return checkNumber(newIndex) ;
+      return checkNumber(newIndex);
     });
   };
+
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
+
   return (
     <article className="review">
       <div className="img-container">
@@ -46,7 +55,9 @@ const Review = () => {
           <FaChevronRight />
         </button>
       </div>
-      <button className="random-btn">suprise me</button>
+      <button className="random-btn" onClick={randomPerson}>
+        suprise me
+      </button>
     </article>
   );
 };
