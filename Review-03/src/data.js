9e371309f274071b434ef50bf0db9c9761bc1f26
FER201m-SP22 <sontddeveloper@gmail.com>
2022-08-14 02:14:52 +0700

    creat-first information

--- Review-03/src/data.js
+++ Review-03/src/data.js
@@ -1,12 +1,12 @@
 const reviews = [
   {
     id: 1,
-    name: 'susan smith',
+    name: 'TRAN DUY SON',
     job: 'web developer',
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      'https://scontent.fsgn2-5.fna.fbcdn.net/v/t39.30808-6/241179475_386570239689319_2237632065155978983_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=TaY-U72yrJMAX96qeld&tn=t5Q6dMD5Q-o4mfNa&_nc_ht=scontent.fsgn2-5.fna&oh=00_AT-2zxlqqXPNebzladjv0s00EVSCHGi2UM9EJygDIrP8rA&oe=62FDBAAA',
     text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+      "I'm Son, a guy who loves developing web, app. I will use what I know to solve the majority of people's complaints in life with technology.",
   },
   {
     id: 2,
